a367dd4582b87ae4a1fd7b8aae1d46d0a7f6d3d0
Ayobami Bamigboye <ayobamibamigboye@Ayobamis-MacBook-Pro.local>
2025-09-19 16:37:41 -0700

    push server files

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#! /bin/env bash
+
+scp -r main.py requirements.txt start.sh model ayobami_bamigboye@pineserver:~/pineserver
---
+++ start.sh
@@ -0,0 +1,21 @@
+#! /bin/env bash
+
+SILENT="2>/dev/null"
+
+start_app(){
+ python -m uvicorn main:app --port 80 --reload
+}
+
+
+install_deps(){
+sudo apt update $SILENT
+sudo apt install python3.11-venv git -y $SILENT
+python3 -m venv venv $SILENT
+. venv/bin/activate
+pip install -r requirements.txt $SILENT
+pip install "fastapi[all]"
+pip install git+https://github.com/BiomedSciAI/biomed-multi-alignment.git $SILENT
+}
+
+install_deps
+start_app
